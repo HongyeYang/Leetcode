232153a99fe4551e8b793d68dad801603cdb959f
Yang,Hongye <hyang37@ur.rochester.edu>
2016-12-12 11:17:45 +0800

    Add files via upload

---
+++ Search a 2D Matrix II.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m=matrix.size(),n=matrix[0].size(),i=0,j=n-1;
+        while(i<m&&j>=0){
+            if(matrix[i][j]==target)return true;
+            else if(matrix[i][j]>target)j--;
+            else i++;
+        }
+        return false;
+    }
+};
