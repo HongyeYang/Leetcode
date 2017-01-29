90ff410bec37d566912ad3cad1b8f7ac017db359
Yang,Hongye <hyang37@ur.rochester.edu>
2017-01-29 09:17:36 +0800

    Add files via upload

---
+++ Number of Segments in a String.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countSegments(string s) {
+        s.push_back(' ');
+        int cnt = 0;
+        for(int i=1;i<s.size();i++){
+            if(s[i]==' '&&s[i-1]!=' ')cnt++;
+        }
+        return cnt;
+    }
+};
