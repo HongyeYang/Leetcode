ba861a1e8c228287ecbc9af8cd706d007efab0e2
Yang,Hongye <hyang37@ur.rochester.edu>
2017-02-11 09:26:07 -0500

    Add files via upload

---
+++ First Missing Positive.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int i=0,n=nums.size();
+        while(i<n){
+            if(nums[i]>0&&nums[i]<n&&nums[i]!=i+1&&nums[i]!=nums[nums[i]-1])swap(nums[i],nums[nums[i]-1]);
+            else i++;
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i+1)return i+1;
+        }
+        return n+1;
+    }
+};
