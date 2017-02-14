f0606327675aed08d3250c44fef4f28f828280b2
Yang,Hongye <hyang37@ur.rochester.edu>
2017-02-13 21:48:35 -0500

    Add files via upload

---
+++ Binary Tree Postorder Traversal(recursion).cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode* root,vector<int>&res){
+        if(!root)return;
+        dfs(root->left,res);
+        dfs(root->right,res);
+        res.push_back(root->val);
+        return;
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int>res;
+        dfs(root,res);
+        return res;
+    }
+};
