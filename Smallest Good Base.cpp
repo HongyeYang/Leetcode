c307b4525004af05110faa752ec706bbb4b07fd2
Yang,Hongye <hyang37@ur.rochester.edu>
2017-02-19 20:20:22 -0500

    Add files via upload

---
+++ Smallest Good Base.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string smallestGoodBase(string n) {
+        unsigned long long tn=(unsigned long long)stoll(n);
+        unsigned long long x=1;
+        for(int i=62;i>=1;i--){
+            if((x<<i)<tn){
+                unsigned long long cur=search(tn,i);
+                if(cur!=0)return to_string(cur);
+            }
+        }
+        return to_string(tn-1);
+    }
+    unsigned long long search(unsigned long long n,int d){
+        double tn=(double)n;
+        unsigned long long left=1;
+        unsigned long long right=(unsigned long long)(pow(n,1.0/d)+1);
+        while(left<=right){
+            unsigned long long mid=left+(right-left)/2;
+            unsigned long long sum=1,cur=1;
+            for(int i=0;i<d;i++){
+                cur*=mid;
+                sum+=cur;
+            }
+            if(sum==n)return mid;
+            else if(sum>n)right=mid-1;
+            else left=mid+1;
+        }
+        return 0;
+    }
+};
