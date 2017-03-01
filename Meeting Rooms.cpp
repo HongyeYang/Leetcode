0ed9169218e8a0f2d346cf257d133d613c63e08d
Yang,Hongye <hyang37@ur.rochester.edu>
2017-03-01 12:56:12 -0500

    Add files via upload

---
+++ Meeting Rooms.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+class Solution {
+public:
+    static bool cmp(Interval i1,Interval i2){
+        return i1.start<i2.start;
+    }
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        sort(intervals.begin(),intervals.end(),cmp);
+        for(int i=1;i<intervals.size();i++){
+            if(intervals[i].start<intervals[i-1].end)return false;
+        }
+        return true;
+    }
+};
