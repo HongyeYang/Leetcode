e8452b7ac4edd85e4f075fcc60667d89af432d72
Yang,Hongye <hyang37@ur.rochester.edu>
2017-03-02 19:56:22 -0500

    Add files via upload

---
+++ Flip Game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<string> generatePossibleNextMoves(string s) {
+        vector<string>res;
+        for(int i=1;i<s.size();i++){
+            if(s[i]=='+'&&s[i-1]=='+'){
+                s[i]='-';
+                s[i-1]='-';
+                res.push_back(s);
+                s[i]='+';
+                s[i-1]='+';
+            }
+        }
+        return res;
+    }
+};
