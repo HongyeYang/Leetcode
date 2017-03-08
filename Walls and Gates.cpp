a9bdec812e0979b6bc198c8e7ed201b50489e578
Yang,Hongye <hyang37@ur.rochester.edu>
2017-03-08 13:21:25 -0500

    Add files via upload

--- Walls and Gates.cpp
+++ Walls and Gates.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
     void wallsAndGates(vector<vector<int>>& rooms) {
+        //deal with the corner cases
         int m=rooms.size();
-        if(!m)return;
+        if(!m)return ;
         int n=rooms[0].size();
         if(!n)return;
-        vector<vector<bool>>used(m,vector<bool>(n,false));
+        
+        vector<int>dirs={-1,0,1,0,-1};
+        //traver the matrix and find the gate as the starting point of dfs
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(rooms[i][j]==INT_MAX)rooms[i][j]=dfs(rooms,i,j);
+                if(rooms[i][j]==0)dfs(rooms,i,j,0,m,n,dirs);
             }
         }
         return;
+        
     }
-    int dfs(vector<vector<int>>&rooms,int i, int j){
-        if(rooms[i][j]!=INT_MAX&&rooms[i][j]>=0)return rooms[i][j];
-        int m=rooms.size(),n=rooms[0].size();
-        int a=m*n;
-        rooms[i][j]=-2;
-        //up
-        if(i>0&&rooms[i-1][j]>=0)a=min(a,dfs(rooms,i-1,j));
-        
-        //down
-        if(i<m-1&&rooms[i+1][j]>=0)a=min(a,dfs(rooms,i+1,j));
-        
-        //left
-        if(j>0&&rooms[i][j-1]>=0)a=min(a,dfs(rooms,i,j-1));
-        //right
-        if(j<n-1&&rooms[i][j+1]>=0)a=min(a,dfs(rooms,i,j+1));
-        
-        rooms[i][j]=INT_MAX;
-        return a==m*n?INT_MAX:a+1;
+    
+    void dfs(vector<vector<int>>&rooms,int i,int j,int cnt,int &m, int &n,vector<int>&dirs){
+        rooms[i][j]=cnt;
+        for(int k=0;k<4;k++){
+            int ni=i+dirs[k];
+            int nj=j+dirs[k+1];
+            if(ni>=0&&ni<m&&nj>=0&&nj<n&&rooms[ni][nj]>cnt+1)dfs(rooms,ni,nj,cnt+1,m,n,dirs);
+        }
+        return;
     }
 };
