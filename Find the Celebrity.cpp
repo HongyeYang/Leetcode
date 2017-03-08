575c8adb5626533fd2f22891b376a29f4e0d5c4a
Yang,Hongye <hyang37@ur.rochester.edu>
2017-03-08 11:43:38 -0500

    Add files via upload

--- Find the Celebrity.cpp
+++ Find the Celebrity.cpp
@@ -4,19 +4,14 @@ bool knows(int a, int b);
 class Solution {
 public:
     int findCelebrity(int n) {
-        if(!n)return -1;
-        if(n==1)return 0;
+        //first pass to find a candidate
         int res=0;
         for(int i=1;i<n;i++){
-            if(res!=-1){
-                if(knows(i,res)&&!knows(res,i))continue;
-                res=-1;
-            }
-            int j=0;
-            for(;j<i;j++){
-                if(!knows(j,i)||knows(i,j))break;
-            }
-            if(i==j)res=i;
+            if(knows(res,i))res=i;
+        }
+        //second pass to validate if the candidate is a real celebrity
+        for(int i=0;i<n;i++){
+            if(i!=res&&(knows(res,i)||!knows(i,res)))return -1;
         }
         return res;
     }
