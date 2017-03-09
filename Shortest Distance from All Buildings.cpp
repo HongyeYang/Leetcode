999ae73a03c5aab0d115bfdb1540cf9aca245512
Yang,Hongye <hyang37@ur.rochester.edu>
2017-03-09 09:02:42 -0500

    Add files via upload

--- Shortest Distance from All Buildings.cpp
+++ Shortest Distance from All Buildings.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int shortestDistance(vector<vector<int>>& grid) {
         int m=grid.size();
-        if(!m)return 0;
+        if(!m)return -1;
         int n=grid[0].size();
-        if(!n)return 0;
+        if(!n)return -1;
         int res=INT_MAX;
+        vector<int>dirs={-1,0,1,0,-1};
         vector<vector<int>>sum(m,vector<int>(n,0));
         int val=0;
-        vector<int>dir{0,1,0,-1,0};
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==1){
@@ -21,21 +21,23 @@ public:
                         int y=q.front().second;
                         q.pop();
                         for(int k=0;k<4;k++){
-                            int ii=x+dir[k];
-                            int jj=y+dir[k+1];
-                            if(ii<0||ii>=m||jj<0||jj>=n||grid[ii][jj]!=val)continue;
-                            grid[ii][jj]--;
-                            dist[ii][jj]=dist[x][y]+1;
-                            sum[ii][jj]+=(dist[ii][jj]-1);
-                            res=min(res,sum[ii][jj]);
-                            q.push({ii,jj});
+                            int ni=x+dirs[k];
+                            int nj=y+dirs[k+1];
+                            if(ni<0||ni>=m||nj<0||nj>=n||grid[ni][nj]!=val)continue;
+                            grid[ni][nj]--;
+                            dist[ni][nj]=dist[x][y]+1;
+                            sum[ni][nj]+=(dist[ni][nj]-1);
+                            res=min(res,sum[ni][nj]);
+                            q.push({ni,nj});
                         }
+                        
                     }
+                    if(res==INT_MAX)return -1;
                     val--;
                 }
+                
             }
         }
-        
         return res==INT_MAX?-1:res;
     }
 };
